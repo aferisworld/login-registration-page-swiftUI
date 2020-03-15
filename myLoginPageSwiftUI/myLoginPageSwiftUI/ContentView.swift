3ad2643e968c807db21aaf396ff279704415f66d
Nana Aferi <githubaferi@gmail.com>
2020-03-15 12:51:10 +0000

    chore(app):Created iOS project

---
+++ myLoginPageSwiftUI/myLoginPageSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  myLoginPageSwiftUI
+//
+//  Created by Nana Aferi on 15/03/2020.
+//  Copyright © 2020 Nana Aferi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
